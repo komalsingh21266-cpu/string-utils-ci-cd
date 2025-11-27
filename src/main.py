d04f735ca504a67908e6cacb9868b6c25a91a1d9
komalsingh21266-cpu <komalsingh21266@gmail.com>
2025-11-27 15:04:37 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from string_utils import reverse_string, to_upper, to_lower, count_vowels
+
+if __name__ == "__main__":
+    sample = "Hello World"
+
+    print("Original:", sample)
+    print("Reverse:", reverse_string(sample))
+    print("Uppercase:", to_upper(sample))
+    print("Lowercase:", to_lower(sample))
+    print("Vowel count:", count_vowels(sample))
+    
+
